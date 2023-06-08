b365213fb5f4da6baba97cdbd237f12133346ab9
Gyuri Kim <133613660+solcasa@users.noreply.github.com>
2023-06-08 14:48:23 +0900

    layout02
    layout02

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $('.main_slide').slick({
+        vertical: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+    })
+});
